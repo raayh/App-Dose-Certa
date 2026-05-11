76b483f1ffec20f9528aa85eeae906b79d3e4b7f
raayh <rayssasac04@gmail.com>
2026-05-11 13:50:41 -0300

    feat(ui): implement medication registration form screen and layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Ionicons } from '@expo/vector-icons';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { View } from 'react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -12,22 +14,68 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: '#65b874ff',
+        tabBarShowLabel: false,
+        tabBarIconStyle: {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        tabBarStyle: { 
+          position: 'absolute',
+          bottom: 30,
+          marginHorizontal: 34,
+
+          height: 70,
+          borderRadius: 40,
+          borderTopWidth: 0,
+
+          elevation: 65,
+          backgroundColor: '#fff',
+          shadowOffset: { width: 0, height: 10 },
+          shadowOpacity: 0.1,
+          shadowRadius: 10,
+
+          paddingBottom: 0,
+          paddingTop: 0,
+         },
         headerShown: false,
-        tabBarButton: HapticTab,
+        // tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />
         }}
       />
+
+      <Tabs.Screen 
+        name="add"
+        options={{
+          title: '',
+          tabBarStyle: { display: 'none'},
+          tabBarIcon: () => (
+            <View style={{
+              backgroundColor: '#65b874ff',
+              width: 60,
+              height: 60,
+              borderRadius: 30,
+              justifyContent: 'center',
+              alignItems: 'center',
+              top: -20,
+            }} > 
+              <Ionicons name="add" size={32} color="#fff" />
+            </View>
+          )
+        }}
+      />
+
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'ellipsis-horizontal',
+          tabBarIcon: ({ color, size }) => <Ionicons name="ellipsis-horizontal" size={size} color={color} />
         }}
       />
     </Tabs>
