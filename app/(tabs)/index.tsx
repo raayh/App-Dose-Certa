4f19329362586c79e01de4b9aaac68f43a40e421
raayh <rayssasac04@gmail.com>
2026-05-11 16:55:06 -0300

    feat(home): establish medication engine and dynamic listing foundation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,198 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Alert, ActivityIndicator, FlatList } from 'react-native';
+import { auth, db } from '@/services/firebaseConfig';
+import { Medication } from '@/types/database';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { signOut } from 'firebase/auth';
+import { CalendarProvider, WeekCalendar } from 'react-native-calendars'
+import { LocaleConfig } from 'react-native-calendars';
+import { Ionicons } from '@expo/vector-icons';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+LocaleConfig.locales['pt-br'] = {
+  monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+  monthNamesShort: ['Jan.','Fev.','Mar','Abr','Mai','Jun','Jul.','Ago','Set.','Out.','Nov.','Dez.'],
+  dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+  dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+  today: 'Hoje'
+};
+LocaleConfig.defaultLocale = 'pt-br';
+
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  const user = auth.currentUser;
+  const [medications, setMedications] = useState<Medication[]>([]);
+  const [loading, setLoading] = useState(true);
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const today = new Date().toISOString().split('T')[0]
+
+  const calendarTheme = {
+    calendarBackground: 'transparent',
+    selectedDayBackgroundColor: 'transparent',
+    selectedDayTextColor: '#65b874',
+    todayTextColor: '#65b874',
+    dayTextColor: '#333',
+    textDayFontFamily: 'Poppins_400Regular',
+    textDayHeaderFontFamily: 'Poppins_400Regular',
+  };
+
+  async function fecthMedications(){
+    if (!user) return;
+
+    try{
+      const medicationQuery = query (
+        collection(db, "medications"), // dizemos qual 'tabela' olhar
+        where("user_id", "==", user.uid)
+      )
+
+      const querySnapshot = await getDocs(medicationQuery)
+      const list = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      } as Medication));
+
+      setMedications(list);
+
+    } catch(error) {
+      Alert.alert("Erro", "Não foi possivelcarregar os dados. Tente novamente mais tarde") 
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      // O vigilante do _layout.tsx vai te tirar daqui automaticamente!
+    } catch (error) {
+      Alert.alert("Erro ao sair", "Não foi possível deslogar.");
+    }
+  };
+
+  useEffect(() => {
+    // 👈 Aqui dentro você dá a partida no motor!
+    fecthMedications();
+  }, []);
+
+
+  return (
+    <CalendarProvider date={today}> 
+      <View style={styles.container}>
+
+        <View style={styles.header}>
+          <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+            <Text style={styles.logoutText}>Sair</Text>
+          </TouchableOpacity>
+        </View>  
+
+        <View style={styles.calendarContainer}>
+          <WeekCalendar 
+            firstDay={1} 
+            theme={calendarTheme}
+          />
+        </View>   
+
+       <View style={styles.content}>
+          {loading ? (
+            <ActivityIndicator size="large" color="#65b874ff" />
+          ) : medications.length === 0 ? (
+            // Caminho A: Lista Vazia!
+            <>
+              <Text style={styles.emptyText}>Você ainda não tem remédios cadastrados.</Text>
+              <Text style={styles.arrow}>↓ Clique no + abaixo</Text>
+            </>
+          ) : (
+            // Caminho B: Achei os remédios! 🎉
+            <FlatList
+              data={medications}
+              keyExtractor={(item) => item.id || Math.random().toString()}
+              contentContainerStyle={styles.listContent}
+              renderItem={({ item }) => (
+                <View style={styles.card}>
+                  <Ionicons> </Ionicons>
+                  <View> 
+                    <Text style={styles.cardTitle}>{item.name}</Text>
+                    <Text style={styles.cardSubtitle}>{item.type} • {item.timesPerDay}x ao dia</Text>
+                  </View>
+                </View>
+              )}
+            />
+          )}
+        </View>
+
+      </View>
+    </CalendarProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    backgroundColor: '#F4F6F5',
+    paddingTop: 30,
+  },
+  header: {
+    paddingHorizontal: 20,
+    alignItems: 'flex-end',
+  },
+  logoutButton: {
+    backgroundColor: '#ff4444',
+    padding: 8,
+    borderRadius: 8
+  },
+  logoutText: {
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
+    paddingBottom: 70
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  calendarContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    paddingVertical: 5,
+    marginTop: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  emptyText: {
+    fontSize: 18,
+    color: '#666',
+    textAlign: 'center'
   },
+  arrow: {
+    fontSize: 14,
+    color: '#65b874ff',
+    marginTop: 10
+  },
+  listContent: {
+    padding: 20,
+    gap: 15,
+    paddingBottom: 100 // Espaço extra para não esconder atrás do menu!
+  },
+  card: {
+    flexDirection: 'row',
+    backgroundColor: '#FFF',
+    padding: 20,
+    borderRadius: 20,
+    width: '100%',
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.05,
+    shadowRadius: 8,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontFamily: 'Poppins_500Medium',
+    color: '#333',
+  },
+  cardSubtitle: {
+    color: '#777',
+    fontSize: 13,
+    marginTop: 4,
+    fontFamily: 'Poppins_400Regular'
+  }
 });
+
